05c8cf197661edfa31fb6a194829feb7c22767e2
FurqatMashrabjonov <php_lesson@mail.ru>
2023-11-11 21:05:20 +0500

    Admin Layout

---
+++ resources/views/livewire/page1.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>Page 1</h1>
+    <br>
+    <a href="/route2" wire:navigate>Page2</a>
+</div>
---
+++ resources/views/livewire/page2.blade.php
@@ -0,0 +1,9 @@
+<div>
+
+    <h1>PAGE 2</h1>
+
+    <br>
+
+    <button wire:click="toPage1">Page1</button>
+
+</div>
